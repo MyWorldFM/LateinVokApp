64b891b776d293a0555bbe23026987062ee7ff41
JuKu <JustinKuenzel@aol.com>
2014-05-19 18:05:08 +0200

    commit

--- LateinVokApp/src/main/java/de/jukusoft/lateinvokapp/MainActivity.java
+++ LateinVokApp/src/main/java/de/jukusoft/lateinvokapp/MainActivity.java
@@ -4,14 +4,28 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    protected ImageButton start_button = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.start_button = (ImageButton) this.findViewById(R.id.startButton);
+
+        this.start_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast toast = Toast.makeText(MainActivity.this, "Click!", Toast.LENGTH_SHORT);
+            }
+        });
     }
 
 
