7c04bcb535532407e3caf78ddc1f97951d7c8b18
JuKu <kuenzel.justin@t-online.de>
2014-05-22 20:35:03 +0200

    Klasse Downloader hinzugefügt.
    Signed-off-by: JuKu <kuenzel.justin@t-online.de>

--- LateinVokApp/src/main/java/de/jukusoft/lateinvokapp/downloader/Downloader.java
+++ LateinVokApp/src/main/java/de/jukusoft/lateinvokapp/downloader/Downloader.java
@@ -1,7 +1,23 @@
 package de.jukusoft.lateinvokapp.downloader;
 
+import android.app.Activity;
+
+import java.io.File;
+
 /**
  * Created by Justin on 19.05.2014.
  */
 public class Downloader {
+
+    protected DownloadFinishedEvent finishedEvent = null;
+    protected File downloadpath = null;
+
+    public Downloader (Activity activity) {
+        //
+    }
+
+    interface DownloadFinishedEvent {
+        public void downloadFinished();
+    }
+
 }
