bfe13df5b2998c5ff5856885a69e5dca051aff70
JuKu <kuenzel.justin@t-online.de>
2014-05-23 19:09:49 +0200

    Klasse Quests und LateinVokAppSerializer hinzugefügt.

---
+++ LateinVokApp/src/main/java/de/jukusoft/lateinvokapp/quests/Quests.java
@@ -0,0 +1,18 @@
+package de.jukusoft.lateinvokapp.quests;
+
+import java.util.List;
+
+/**
+ * Created by Justin on 23.05.2014.
+ */
+public class Quests {
+
+    public List<Quests> listQuests () {
+        return null;
+    }
+
+    public List<String> getModules () {
+        return null;
+    }
+
+}
---
+++ LateinVokApp/src/main/java/de/jukusoft/lateinvokapp/serializer/LateinVokAppSerializer.java
@@ -0,0 +1,18 @@
+package de.jukusoft.lateinvokapp.serializer;
+
+import java.io.File;
+
+/**
+ * Created by Justin on 23.05.2014.
+ */
+public class LateinVokAppSerializer {
+
+    public static boolean serialize (String path, Object object) {
+        return true;
+    }
+
+    public static Object unserialize (String path) {
+        return null;
+    }
+
+}
