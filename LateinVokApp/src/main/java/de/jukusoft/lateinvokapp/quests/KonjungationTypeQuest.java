3a9e47bcd3e63290d8cc871a6ff6d725e50b521f
JuKu <kuenzel.justin@t-online.de>
2014-05-26 17:35:45 +0200

    Layout quest_konjungationtype.xml hinzugefügt, ID relativelayout3 hinzugefügt, RelativeLayout3 gefixt, KonjungationTypeQuest und Quest geändert.

--- LateinVokApp/src/main/java/de/jukusoft/lateinvokapp/quests/KonjungationTypeQuest.java
+++ LateinVokApp/src/main/java/de/jukusoft/lateinvokapp/quests/KonjungationTypeQuest.java
@@ -1,16 +1,51 @@
 package de.jukusoft.lateinvokapp.quests;
 
+import android.app.Activity;
+import android.content.Context;
+import android.widget.Button;
+
+import java.util.HashMap;
+
+import de.jukusoft.lateinvokapp.R;
+
 /**
  * Created by Justin on 24.05.2014.
  */
 public class KonjungationTypeQuest extends Quest {
 
+    protected String lateinword = "";
+    protected KONJUNGATIONTYPE correct_konj = null;
+
+    public enum KONJUNGATIONTYPE {A_Konj, E_Konj, I_Konj, KonsKonj1, KonsKonj2};
+
     public KonjungationTypeQuest() {
-        //
+        //Quest, bei dem man die richtige Konjungationsart eines lat. Verb auswählen muss.
     }
 
-    public void loadQuests () {
-        //
+    @Override
+    public void loadQuest () {
+        if ((this.getExtra("lateinword")) != null) {
+            this.lateinword = (String) this.getExtra("lateinword");
+        } else {
+            //Neues lat. Verb holen
+            this.lateinword = "";
+        }
+    }
+
+    @Override
+    public void showQuest (Activity activity) {
+        activity.setContentView(R.layout.quest_konjungationtype);
+    }
+
+    @Override
+    public boolean check (Activity activity, HashMap<String, Object> data) {
+        Button clicked_button = (Button) data.get("button");
+
+        if (this.correct_konj.equals(clicked_button.getText())) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
 }
--- LateinVokApp/src/main/java/de/jukusoft/lateinvokapp/quests/Quest.java
+++ LateinVokApp/src/main/java/de/jukusoft/lateinvokapp/quests/Quest.java
@@ -1,5 +1,10 @@
 package de.jukusoft.lateinvokapp.quests;
 
+import android.app.Activity;
+import android.content.Context;
+import android.view.View;
+
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -7,12 +12,42 @@ import java.util.List;
  */
 public class Quest {
 
-    public List<Quest> listQuests () {
-        return null;
+    protected HashMap<String,Object> extra = new HashMap<String,Object>();
+
+    public Quest () {
+        //
     }
 
-    public List<String> getModules () {
-        return null;
+    public void loadQuest () {
+        //
+    }
+
+    public void showQuest (Activity activity) {
+        //
+    }
+
+    public boolean check (Activity activity, HashMap<String,Object> data) {
+        return true;
+    }
+
+    public void putExtra (String str, Object obj) {
+        this.extra.put(str, obj);
+    }
+
+    public Object getExtra (String str) {
+        if (this.extra.containsKey(str)) {
+            return this.extra.get(str);
+        } else {
+            return null;
+        }
+    }
+
+    public class QuestClickListener implements View.OnClickListener {
+
+        @Override
+        public void onClick(View view) {
+            //
+        }
     }
 
 }
